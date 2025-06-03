72619933233234d11f10dc5d82f7baf15ea06c44
Lorenzo Curreli <lorenzo.curreli@spindox.it>
2025-06-03 09:27:58 +0200

    chore: add global context

--- packages/demo/packages/backend/global.d.ts
+++ packages/demo/packages/backend/global.d.ts
@@ -1,25 +1,7 @@
 // global.d.ts
-
-export {};
+import { Context } from '@flowerforce/flowerbase';
+export { };
 
 declare global {
-  const context: {
-    services: {
-      get: (name: string) => {
-        db: (dbName: string) => {
-          collection: <T = any>(name: string) => {
-            find: (query?: any) => {
-              skip: (n: number) => any;
-              limit: (n: number) => any;
-              toArray: () => Promise<T[]>;
-            };
-            countDocuments: (query?: any) => Promise<number>;
-          };
-        };
-      };
-    };
-    user: {
-      id: string;
-    };
-  };
+  const context: Context
 }
