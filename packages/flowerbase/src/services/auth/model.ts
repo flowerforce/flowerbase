353c03017b2c9820c5342257b121295f46e3e74a
Andrea Zucca <andrea.zucca@spindox.it>
2026-01-23 17:14:13 +0100

    fix: add delete auth user method

--- packages/flowerbase/src/services/auth/model.ts
+++ packages/flowerbase/src/services/auth/model.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from "fastify/types/instance"
+import handleUserDeletion from "../../../src/shared/handleUserDeletion"
 import handleUserRegistration from "../../../src/shared/handleUserRegistration"
 import { Options } from "../../shared/models/handleUserRegistration.model"
 
 type EmailPasswordAuth = {
     registerUser: ReturnType<typeof handleUserRegistration>
+    deleteUser: ReturnType<typeof handleUserDeletion>
 }
 
 type AuthMethods = {
