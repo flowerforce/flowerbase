fdb67c8817ba08b14f34230f1baa0819e962ab38
jessicalussu <jessica.lussu@spindox.it>
2026-02-17 12:19:45 +0100

    chore: clean users-exporter

--- packages/users-exporter/utils.js
+++ packages/users-exporter/utils.js
@@ -5,7 +5,7 @@ export const loadUsers = async ({
   lastUser,
   cookie,
   traceId,
-  password = 'temp-password'
+  password
 }) => {
   const users = []
   const BASE_URL = `https://services.cloud.mongodb.com/api/admin/v3.0/groups/${groupId}/apps/${appId}/users?`
@@ -14,7 +14,7 @@ export const loadUsers = async ({
     headers: {
       accept: 'application/json',
       'accept-language': 'it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7',
-      baggage: `sentry-environment=prod,sentry-release=4.115.0,sentry-public_key=db073c15b6c14ecf82cbf7ac43e037f3,sentry-trace_id=${traceId},sentry-sample_rate=0,sentry-sampled=false`,
+      baggage: `sentry-environment=prod,sentry-release=4.115.0,sentry-public_key=xxxxxx,sentry-trace_id=${traceId},sentry-sample_rate=0,sentry-sampled=false`,
       'content-type': 'application/json',
       priority: 'u=1, i',
       'sec-ch-ua': '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
@@ -26,8 +26,7 @@ export const loadUsers = async ({
       'sentry-trace': `${traceId}`,
       'x-baas-request-origin': 'mongodb-baas-ui',
       cookie: cookie,
-      Referer:
-        'https://services.cloud.mongodb.com/groups/600daac31bee660769895af1/apps/600dac958546a86b86a90927/auth/users',
+      Referer: 'https://services.cloud.mongodb.com/groups/xxxx/apps/xxxxx/auth/users',
       'Referrer-Policy': 'strict-origin-when-cross-origin'
     },
     body: null,
