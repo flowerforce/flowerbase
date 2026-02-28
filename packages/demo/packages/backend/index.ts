6b6449278e84cc9866b1e01bdd8004bb7d6a581f
Gianluca Spada <gianluca.spada@stackhouse.it>
2026-02-28 15:40:23 +0100

    feat(demo): add CSFLE config

--- packages/demo/packages/backend/index.ts
+++ packages/demo/packages/backend/index.ts
@@ -11,5 +11,31 @@ initialize({
     port,
     mongodbUrl,
     jwtSecret,
-    host
+    host,
+    mongodbEncryptionConfig: {
+        // extraOptions: {
+        //     cryptSharedLibPath: "__path_to_crypt_shared__"
+        // },
+        kmsProviders: [
+            {
+                type: "aws",
+                keyAlias: "prod-data-key",
+                config: {
+                    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+                    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
+                },
+                masterKey: {
+                    key: "arn:aws:kms:eu-central-1:123456789:key/123456789",
+                    region: "eu-central-1"
+                }
+            },
+            {
+                type: "local",
+                keyAlias: "dev-data-key",
+                config: {
+                    key: "DE1+JeO3S2BTXar3FScBFsSnuz1TjhppXfqi9IBNA2JmVJx9lkBtcDu13/uzzL78r16iVeKKKLgzfOrXLrU+OqfjbaqzugOSbF/1I1q8pZP29vMzl625Thb2s1QEgMlF"
+                },
+            }
+        ]
+    }
 })
